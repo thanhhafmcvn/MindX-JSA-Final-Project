0dd7e3853ad4f2c7e7013d8f06beaea1f1c51ee2
mlucastero <thanhhafmcvn@outlook.com.vn>
2023-01-25 14:19:31 +0700

    Uploaded the login/signup page to Github

---
+++ js/first.js
@@ -0,0 +1,39 @@
+
+const signup = () => {
+  const signup_email = document.querySelector("#sign-up_email");
+  const signup_username = document.querySelector("#sign-up_username");
+  const signup_password = document.querySelector("#sign-up_password");
+  const signup_retype = document.querySelector("#sign-up_retype");
+  const user = {
+    email: signup_email.value,
+    username: signup_username.value,
+    password: signup_password.value,
+  };
+    if (signup_password.value !== signup_retype.value) {
+      alert('Vui lòng nhập lại chính xác mật khẩu')
+    }
+    else {
+        console.log('pass')
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+
+};
+const signin = () => {
+    const signin_email_username = document.querySelector("#sign-in_username-email");
+    const signin_password = document.querySelector("#sign-in_password");
+    console.log(localStorage.getItem('user'));
+    console.log(signin_email_username.value)
+    const signinData = JSON.parse(localStorage.getItem('user'))
+    if (signinData.username === signin_email_username.value || signinData.email === signin_email_username.value) {
+        if (signinData.password === signin_password.value) {
+            window.location = '../html/home.html'
+            console.log('passs');
+        }
+        else {
+            alert("Mật khẩu của bạn đã bị sai")
+        }
+    }
+    else {
+        alert('Người dùng không tồn tại')
+    }
+}
